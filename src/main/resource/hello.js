f6db86c3cd0aa413b64a2ff33e719095dfeb0991
beom <bum4303@naver.com>
2020-04-14 11:10:50 +0900

    initial commit

---
+++ src/main/resource/hello.js
@@ -0,0 +1,25 @@
+function max(arg1, arg2) {
+    let arg1isLarger = arg1 > arg2;
+    console.log(arg1isLarger)
+    if (arg1isLarger) {
+        return arg1;
+    } else {
+        return arg2;
+    }
+}
+
+function max2(arg1, arg2, arg3) {
+    return max(max(arg1, arg2), arg3)
+}
+
+class Parent {
+
+}
+
+class Child extends Parent {
+
+}
+
+class Child2 extends Child {
+
+}
